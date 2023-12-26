12f5c7d06851ddd1ecf97247a6920a609226ba8b
Mohammad Abdlrahman Sad <91919000+Mohammad-abdo@users.noreply.github.com>
2023-12-26 11:16:41 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,14 +8,14 @@ interface props{
   productData:productProps
 }
 
-export default function Home({productDate}:props) {
+export default function Home({productData}:props) {
 
   return (
     <main className="" >
 <div className="max-w-screen-2xl mx-auto">
   <Banner/>
 <div className=" relative md:-mt-20 xl:-mt-60 lgl:-mt-32 z-20 mb-10">
-<Products productDate={productDate}/>
+<Products productData={productData}/>
 </div>
 </div>
     </main>
@@ -26,6 +26,6 @@ export const getServerSideProps=async () => {
   const res=await fetch("https://fakestoreapiserver.reactbd.com/tech")
  const productDate=await res.json()
 
- return {props:{productDate}}
+ return {props:{productData}}
   
 }
